d779244511781f0f0102e85a991530bd32795b23
Samuel Tardieu <sam@rfc1149.net>
2024-06-20 21:07:58 +0200

    New lint `deferred_const_shadow`

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -112,6 +112,7 @@ mod default_constructed_unit_structs;
 mod default_instead_of_iter_empty;
 mod default_numeric_fallback;
 mod default_union_representation;
+mod deferred_const_shadow;
 mod dereference;
 mod derivable_impls;
 mod derive;
@@ -950,5 +951,6 @@ pub fn register_lints(store: &mut rustc_lint::LintStore, conf: &'static Conf) {
     store.register_late_pass(|_| Box::new(cloned_ref_to_slice_refs::ClonedRefToSliceRefs::new(conf)));
     store.register_late_pass(|_| Box::new(copy_then_borrow_mut::CopyThenBorrowMut::new(conf)));
     store.register_late_pass(|_| Box::new(unit_as_impl_trait::UnitAsImplTrait));
+    store.register_late_pass(|_| Box::<deferred_const_shadow::DeferredConstShadow<'_>>::default());
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
