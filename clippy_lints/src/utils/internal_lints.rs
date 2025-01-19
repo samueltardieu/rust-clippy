b7c8d7322bb88784e82fb0ad624c37435e8366d7
Samuel Tardieu <sam@rfc1149.net>
2025-01-19 10:17:42 +0100

    New internal lint: `manual_span_operation`

--- clippy_lints/src/utils/internal_lints.rs
+++ clippy_lints/src/utils/internal_lints.rs
@@ -3,6 +3,7 @@ pub mod collapsible_calls;
 pub mod interning_defined_symbol;
 pub mod invalid_paths;
 pub mod lint_without_lint_pass;
+pub mod manual_span_operation;
 pub mod msrv_attr_impl;
 pub mod outer_expn_data_pass;
 pub mod produce_ice;
