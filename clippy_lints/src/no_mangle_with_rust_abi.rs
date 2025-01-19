ba3fc68c1bc192dd038d83e0cf5eade56d090a5f
Samuel Tardieu <sam@rfc1149.net>
2025-01-19 11:22:06 +0100

    Apply `manual_span_operation` to Clippy sources

--- clippy_lints/src/no_mangle_with_rust_abi.rs
+++ clippy_lints/src/no_mangle_with_rust_abi.rs
@@ -42,7 +42,7 @@ impl<'tcx> LateLintPass<'tcx> for NoMangleWithRustAbi {
         {
             let attrs = cx.tcx.hir().attrs(item.hir_id());
             let mut app = Applicability::MaybeIncorrect;
-            let fn_snippet = snippet_with_applicability(cx, fn_sig.span.with_hi(item.ident.span.lo()), "..", &mut app);
+            let fn_snippet = snippet_with_applicability(cx, fn_sig.span.until(item.ident.span), "..", &mut app);
             for attr in attrs {
                 if let Some(ident) = attr.ident()
                     && ident.name == rustc_span::sym::no_mangle
